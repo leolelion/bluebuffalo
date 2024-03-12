243b2138605eb06d2cdd0f692b542d3cdf93c8c7
leolelion <144266057+leolelion@users.noreply.github.com>
2024-03-12 08:31:37 -0700

    app.config for database, import sqlalchemy

--- DashboardProject/__init__.py
+++ DashboardProject/__init__.py
@@ -1,14 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 from os import path
 
 # db = SQLAlchemy()
-# DB_NAME = 'database.db'
+DB_NAME = 'database.db'
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'r'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}]'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
     from .views import views
     from .auth import auth
