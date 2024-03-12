8d1ff1aec3c7abfb7804583a325e91d7a5c6fa49
vol2468 <vol2468@student.ubc.ca>
2024-03-12 11:35:24 -0700

    updated main, models, database

--- DashboardProject/models.py
+++ DashboardProject/models.py
@@ -7,23 +7,25 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(100))
     password = db.Column(db.String(100))
     city = db.Column(db.String(100))
-    latitude = db.Column(db.String(100))
-    longitude = db.Column(db.String(100))
 
 
-"""
+
 class City(db.Model):
     name = db.Column(db.String(100), primary_key=True, unique=True)
+    latitude = db.Column(db.String(100))
+    longitude = db.Column(db.String(100))
+    population = db.Column(db.String(100))
     graphId = db.Column(db.Integer)
     graphDescription = db.Column(db.String(10000))
 
 
 class Comment(db.Model):
     commentId = db.Column(db.String(100), primary_key=True, unique=True)
+    user = db.Column(db.String(100))
     commentText = db.Column(db.String(10000))
     commentDate = db.Column(db.Date)
 
-"""
+
 
 
 
