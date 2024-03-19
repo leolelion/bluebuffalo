bd032c83a2023ba13c5aaab75e60b07ff7bf6fa4
puyangogju <119086209+puyangogju@users.noreply.github.com>
2024-03-19 08:52:15 -0700

    Update auth.py

--- DashboardProject/auth.py
+++ DashboardProject/auth.py
@@ -17,7 +17,22 @@ def index():
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
 
 @auth.route('/newAccount')
 def newAccount():
