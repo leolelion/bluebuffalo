6042bbb7b5ffb8e566c6bc5ddf0ec7a516929c86
puyangogju <119086209+puyangogju@users.noreply.github.com>
2024-03-22 18:40:09 -0700

    updated auth py, analysis and login
    updated the login .py and also made the analysis.py to minimize conflict

--- DashboardProject/auth.py
+++ DashboardProject/auth.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, render_template, request, flash, url_for, Blueprint
+from DashboardProject.analysis import perform_analysis
 
 auth = Blueprint('auth', __name__)
 
@@ -7,46 +8,44 @@ auth = Blueprint('auth', __name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+
 @auth.route('/header')
 def header():
     return render_template("header.html")
+
 
 @auth.route('/index')
 def index():
     return render_template("index.html")
 
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
+    result = login()
+    return result
 
-        user = User.query.filter_by(email=email).first()
-        if user:
-            if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
-                login_user(user, remember=True)
-                return redirect(url_for('views.home'))
-            else:
-                flash('Incorrect password, try again.', category='error')
-        else:
-            flash('Email does not exist.', category='error')
 
-    return render_template("login.html", user=current_user)
 
 @auth.route('/newAccount')
 def newAccount():
     return render_template("newAccount.html")
 
+
 @auth.route('/accountSetting')
 def accountSetting():
     return render_template("accountSetting.html")
+
 
 @auth.route('/map')
 def map():
     return render_template("map.html")
 
-@auth.route('/analysis')
-def analysis():
-    return render_template('analysis.html')
 
+@auth.route('/analysis', methods=['GET', 'POST'])
+def call_compare():
+    result = perform_analysis()
+    return result
+
+@auth.route('/test')
+def test():
+    return render_template('test.html')
---
+++ DashboardProject/login.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
