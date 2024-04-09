a877f401d0958a167c4779b71a31cab9a58f6d16
leolelion <leokaiya@proton.me>
2024-04-09 08:03:33 -0700

    auth reformating

--- DashboardProject/auth.py
+++ DashboardProject/auth.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, Blueprint, redirect, url_for
-from DashboardProject.analysis import perform_analysis, get_latitude, get_longitude, get_mean_values, get_total_mean, get_prediction
+from DashboardProject.analysis import perform_analysis, get_latitude, get_longitude, get_mean_values, get_total_mean, \
+    get_prediction
 from DashboardProject.dashboard import get_top10_data, get_least10_data, get_pollutant_data, get_aqi_population
 from DashboardProject.map import perform_map
 from DashboardProject.insertComment import insert_comment
@@ -9,10 +10,13 @@ from DashboardProject.login import check_login
 from DashboardProject.message import email_alert
 
 auth = Blueprint('auth', __name__)
+
+
 @auth.route('/')
 def hello_world():  # put application's code here
     """"""
     return 'Hello World!'
+
 
 @auth.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
@@ -22,35 +26,42 @@ def dashboard():
     pollutant = get_pollutant_data(user_date)
     aqi_population = get_aqi_population(user_date)
     comment = display_comment_index(user_date)
-    return render_template('index.html', top_cities=top_cities, least_cities=least_cities,\
-                            aqi_population=aqi_population, pollutant=pollutant,\
-                                user_date=user_date, comments=comment)
+    return render_template('index.html', top_cities=top_cities, least_cities=least_cities, \
+                           aqi_population=aqi_population, pollutant=pollutant, \
+                           user_date=user_date, comments=comment)
+
 
 @auth.route('/header')
 def header():
     return render_template("header.html")
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     result = check_login()
     return result
 
+
 @auth.route('/newAccount')
 def newAccount():
     return render_template("newAccount.html")
 
-@auth.route('/accountSetting')
+
+@auth.route('/accountSetting', methods=['GET', 'POST'])
 def accountSetting():
     return render_template("accountSetting.html")
+
 
 @auth.route('/map', methods=['GET', 'POST'])
 def map():
     result = perform_map()
     return result
 
+
 @auth.route('/map_test')
 def map_test():
     return render_template('map_test.html')
+
 
 @auth.route('/latitude', methods=['GET', 'POST'])
 def latitude():
@@ -58,11 +69,13 @@ def latitude():
     latitude = get_latitude(city)
     return latitude
 
+
 @auth.route('/longitude', methods=['GET', 'POST'])
 def longitude():
     city = request.form.get('city')
     longitude = get_longitude(city)
     return longitude
+
 
 @auth.route('/analysisMean', methods=['GET', 'POST'])
 def analysis_mean():
@@ -70,11 +83,13 @@ def analysis_mean():
     mean = get_mean_values(city)
     return mean
 
+
 @auth.route('/analysisTotal', methods=['GET', 'POST'])
 def analysis_total():
     city = request.form.get('city')
     total = get_total_mean(city)
     return total
+
 
 @auth.route('/analysisPrediction', methods=['GET', 'POST'])
 def analysis_prediction():
@@ -84,15 +99,20 @@ def analysis_prediction():
     prediction_list = prediction.tolist()
     return prediction_list
 
+
 @auth.route('/testMessage', methods=['GET', 'POST'])
 def testMessage():
     recipient = request.form.get('email')
-    result = email_alert("Account Confirmation", "Thank you for creating an account! We're happy to have you here! Enjoy learning about Air Quality!", recipient)
+    result = email_alert("Account Confirmation",
+                         "Thank you for creating an account! We're happy to have you here! Enjoy learning about Air Quality!",
+                         recipient)
     return redirect(url_for('auth.dashboard'))
+
 
 @auth.route('/test', methods=['GET', 'POST'])
 def test():
     return render_template('test.html')
+
 
 @auth.route('/insertComment', methods=['GET', 'POST'])
 def insertComment_analysis():
